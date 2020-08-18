d15f0313b05ef857d139344ac06f9b9d71937592
Alma <a.q.frankenstein@gmail.com>
2020-08-18 09:40:05 -0700

    change forEach to for loop
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   const favIceCreams = ["Coffee", "Pistachio", "Rocky Road"];
 
-  favIceCreams.forEach(function(favIceCream) {
-    $("#iceCreams").append(" " + favIceCream + "!");
-  });
+  // favIceCreams.forEach(function(favIceCream) {
+  //   $("#iceCreams").append(" " + favIceCream + "!");
+  // });
+  for (let index = 0; index < favIceCreams.length; index += 1){
+    $('#iceCreams').append(" " +favIceCreams[index]+"!");
+  }
 });
